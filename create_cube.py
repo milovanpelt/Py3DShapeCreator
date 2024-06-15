e531c87c552804a9d7d322a01a44043d2566e5d3
Milo van Pelt <milovanpelt@gmail.com>
2024-06-15 18:37:14 +0200

    [CHANGED] Cube is created using the Cube class

--- create_cube.py
+++ create_cube.py
@@ -1,7 +1,15 @@
+import sys
+import os
+
+# Add the directory of the current script to the system path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import bpy
+from cube import Cube
 
 def create_cube():
-    bpy.ops.mesh.primitive_cube_add(size=10, enter_editmode=False, align='WORLD', location=(0,0,0))  
+    Cube(0, 0, 0, 10)
+    bpy.ops.wm.save_mainfile()
 
 if __name__ == "__main__":
     create_cube()
--- cube.py
+++ cube.py
@@ -2,4 +2,4 @@ import bpy
 
 class Cube:
     def __init__(self, x, y, z, _size):
-        bpy.ops.mesh.primitive_cube_add(size=_size, enter_editmode=False, align='WORLD', location=(x, y, z))  
+        bpy.ops.mesh.primitive_cube_add(size=_size, enter_editmode=False, align='WORLD', location=(x, y, z))
